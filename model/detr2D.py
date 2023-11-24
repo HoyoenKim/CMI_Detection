6cbd4fdaf4922aea373766e17de7fb81e8b7eef7
HoyeonKim <cabinkhy@gm.gist.ac.kr>
2023-11-25 04:31:14 +0900

    change detr model

--- model/detr2D.py
+++ model/detr2D.py
@@ -114,8 +114,7 @@ class DETRLoss(nn.Module):
         self.bce = nn.BCEWithLogitsLoss()
         self.reg_loss = nn.GaussianNLLLoss(reduction="none")
 
-    def forward(self, model_output: torch.Tensor, labels: torch.Tensor):
-        logits = model_output.logits
+    def forward(self, logits: torch.Tensor, labels: torch.Tensor):
         indices = self.matcher(logits[:, :, [0, 1, 3]], labels)
 
         loss_boxes = self.loss_boxes(logits, labels, indices)
