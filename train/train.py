5b22ba162b3d9a042fe82f30491df571afdb8cbf
HoyeonKim <cabinkhy@gm.gist.ac.kr>
2023-11-26 23:24:17 +0900

    revise CenterNet train

--- train/train.py
+++ train/train.py
@@ -112,7 +112,7 @@ def train(cfg, env):
 def do_train(model, dataloader, optimizer, criterion, device, cfg):
     model.train()
     total_loss = 0
-    for i, batch in MyProgressBar(enumerate(dataloader), total=len(dataloader)):
+    for i, batch in tqdm(enumerate(dataloader), total=len(dataloader)):
         inputs, labels = batch['feature'].to(device), batch['label'].to(device)
 
         optimizer.zero_grad()
@@ -131,7 +131,7 @@ def do_validate(model, dataloader, criterion, device, cfg):
     model.eval()
     total_loss = 0
     with torch.no_grad():
-        for i, batch in MyProgressBar(enumerate(dataloader), total=len(dataloader)):
+        for i, batch in tqdm(enumerate(dataloader), total=len(dataloader)):
             inputs, labels = batch['feature'].to(device), batch['label'].to(device)
             do_mixup = np.random.rand() < cfg["aug"]["mixup_prob"]
             do_cutmix = np.random.rand() < cfg["aug"]["cutmix_prob"]
